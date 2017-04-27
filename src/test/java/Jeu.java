5649c9f48ef17d9bf635f8e0bd016196ae46ab8e
charlotteleon <charlotte.leon@open-groupe.com>
2017-04-27 09:58:10 +0200

    update junit

--- src/test/java/Jeu.java
+++ src/test/java/Jeu.java
@@ -1,12 +1,26 @@
 package test.java;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class Jeu {
 
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println(args[0]);
 		System.out.println(args[1]);
 		System.out.println("truc");
+
+
+	}*/
+	
+	@Test
+	public void jeutest(){
+		String str ="OK";
+		assertEquals("OK", str);
+		System.out.println("tester");
 	}
 
 }
