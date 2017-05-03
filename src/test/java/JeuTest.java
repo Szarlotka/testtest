07968dd8b690354ffe32531b78a3aac186f2543e
charlotteleon <charlotte.leon@open-groupe.com>
2017-05-03 14:03:16 +0200

    test3 Selenium

--- src/test/java/JeuTest.java
+++ src/test/java/JeuTest.java
@@ -5,6 +5,8 @@ import static org.testng.Assert.assertEquals;
 import java.io.IOException;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 import org.testng.annotations.AfterSuite;
@@ -13,22 +15,10 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
-//import static org.junit.Assert.assertEquals;
-//import org.junit.Test;
 
 public class JeuTest {
 
-	/*public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-		System.out.println("truc");
-
-
-	}
-	 */
-
+	
 	@BeforeSuite
 	public void setup () throws InvalidFormatException, IOException{
 		DataTest data = new DataTest();
@@ -42,14 +32,27 @@ public class JeuTest {
 		assertEquals(mot, "OK");
 		System.out.println("test1");
 	}
-	
+
 	@Test
 	public void test2 (){
 		System.out.println("test2");
 	}
 
+	@Test
+	public void test3 (){
+		System.out.println("test3");
+		String baseUrl = "http://newtours.demoaut.com/";
+		String driverPath = "C:\\geckodriver.exe";
+		WebDriver driver ; 
+		System.out.println("launching firefox browser"); 
+		System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\UFTFormation\\Downloads\\geckodriver-v0.15.0-win32\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get(baseUrl);
+	}
+
 	@AfterSuite
 	public void teardown(){
-		
+
 	}
 }
